bb10afaa226539e42ff7374aac13a2d4df8642ff
Mateus Peres <mateuspereslopesl@outlook.com>
2024-09-20 03:54:35 -0300

    refactoring: removendo pastas da branch database

--- assets/app/scripts/scripts.js
+++
@@ -1,4 +0,0 @@
-const myH2 = document.createElement("h2");
-myH2.innerHTML = "Aplicação do Usuário";
-
-document.querySelector("h1").insertAdjacentElement("afterend",myH2);
--- source/Support/readme.md
+++
@@ -1,2 +0,0 @@
-## Pasta Support
-> Pasta para armazenar classes criadas a partir de módulos esxternos, por exemplo a DomPDF.
